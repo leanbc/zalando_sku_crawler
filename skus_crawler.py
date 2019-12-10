637dcbedbdb5599aabf88ee7961201dcbfbc53a8
Leandro Bleda Cantos <leandro.bleda-cantos@klarna.com>
2019-12-10 23:36:16 +0100

    "adding config and logging"

--- skus_crawler.py
+++ skus_crawler.py
@@ -2,83 +2,49 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import pandas as pd
-import sys
+import configparser
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+url=config.get('URL', 'url')
+path=config.get('PATH', 'path')
+
+logging.info('We will get data from :' + url)  # will print a message to the console
 
 
 
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+response = requests.get(url, headers={'User-Agent': user_agent})
+soup = BeautifulSoup(response.text, 'html.parser')
+textContent = []
+paragraphs = soup.find_all('script',attrs={"id":"z-nvg-cognac-props","type":"application/json"})[0].text
+textContent.append(paragraphs)
+b=json.loads(textContent[0][9:-3])
+df=pd.DataFrame()
 
-def main():
 
+for page in range(1,b['pagination']['page_count']+1):
 
-    # url = 'https://www.zalando.es/collections/2NcExSbAQzi/?p='
-    url=sys.argv[1] + '?p='
-    print('URL:',url)
-    path_to_csv=sys.argv[2]
-    print('path_to_csv:',path_to_csv)
-
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-
-    response = requests.get(url, headers={'User-Agent': user_agent})
-    soup = BeautifulSoup(response.text, 'html.parser')
-    soup.find('script',attrs={"id":"z-nvg-cognac-props","type":"application/json"})
-    textContent = []
-    for i in range(0,1):
-        paragraphs = soup.find_all('script',attrs={"id":"z-nvg-cognac-props","type":"application/json"})[i].text
-        textContent.append(paragraphs)
-        a=json.loads(textContent[0][9:-3])
-
+    new_url=url+'?p=' + str(page)
+    logging.info('paginatition : ' + new_url)
     l=[]
 
-    for i in a['articles']:
+    for i in b['articles']:
         data=dict()
         data['sku'] =i['sku']
         data['price'] =i['price']['promotional']
         data['brand_name'] =i['brand_name']
-        data['url'] =url
+        data['url'] =new_url
         l.append(data)
 
-
-    df=pd.DataFrame(l)
-
-
-    if a['pagination']['page_count']>1:
-        for page in range(2,a['pagination']['page_count']+1):
-
-            next_url= url + str(page)
-            print(next_url)
-            user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-
-            response = requests.get(next_url, headers={'User-Agent': user_agent})
-            soup = BeautifulSoup(response.text, 'html.parser')
-            soup.find('script',attrs={"id":"z-nvg-cognac-props","type":"application/json"})
-            textContent = []
-            for i in range(0,1):
-                paragraphs = soup.find_all('script',attrs={"id":"z-nvg-cognac-props","type":"application/json"})[i].text
-                textContent.append(paragraphs)
-                a=json.loads(textContent[0][9:-3])
+    df=df.append(pd.DataFrame(l), ignore_index = True)
 
 
-            l=[]
+df.to_csv(path,header=True)
 
-            for i in a['articles']:
-                data=dict()
-                data['sku'] =i['sku']
-                data['price'] =i['price']['promotional']
-                data['brand_name'] =i['brand_name']
-                data['url'] =next_url
-                l.append(data)
-
-            df1=pd.DataFrame(l)
-
-            df=df.append(df1, ignore_index = True)
-
-
-
-    df.to_csv(path_to_csv,header=True)
-
-
-
-
-if __name__ == '__main__':
-
-    main()
+logging.info('FINISH!!!!-- The file is in:' + str(path))
+logging.info('Number of Skus:' + str(df.brand_name.count()))
